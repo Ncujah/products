47d2d8dd256fd2c4ae9d4b0c68a0fa0c05561c9c
Ncuuuu <booshhof@gmail.com>
2020-04-01 00:49:18 +0200

    Add files via upload

---
+++ Users.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data.Odbc;
+
+namespace Products
+{
+    public partial class Users : frmInheritance
+    {
+        string strUserName;
+        string strFirstName;
+        string strLastName;
+        string strPassword;
+        int intUserID=0;
+       
+        bool boolUserExists = false;
+       
+        string strAccessConnectionString = "Driver= {Microsoft Access Driver (*.mdb)}; Dbq=Products.mdb; Uid=Admin; Pwd=;";
+        
+        public Users()
+        {
+            InitializeComponent();
+        }
+
+        private void Users_Load(object sender, EventArgs e)
+        {
+            controlsLoad();
+            loadUsers();
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            frmMain frmMain = new frmMain();
+            frmMain.Show();
+            this.Hide();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (btnCreate.Text == "Save")
+            {
+                if (txtUserName.Text == "")
+                {
+                    MessageBox.Show("User Name cannot be left empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (txtPassword.Text == "")
+                {
+                    MessageBox.Show("Password field cannot be left empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (txtFirstName.Text == "")
+                {
+                    MessageBox.Show("First Name field cannot be left empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (txtLastName.Text == "")
+                {
+                    MessageBox.Show("Last Name field cannot be left empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+
+                    checkIfUsersExists();
+                    if (boolUserExists == false)
+                    {
+                        createUser();
+                        controlsLoad();
+                        ClearTextBoxes();
+                        loadUsers();
+                    }
+                    else if (boolUserExists == true)
+                    {
+                        MessageBox.Show("User already exists", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            else if (btnCreate.Text == "Create")
+            {
+                controlsCreate();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            controlsEdit();
+            editUser();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            updateUser();
+            controlsLoad();
+            ClearTextBoxes();
+            loadUsers();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            controlsLoad();
+            ClearTextBoxes();
+            loadUsers();
+            deleteUser();
+
+        }
+        private void controlsLoad()
+        {
+            txtUserName.Enabled = false;
+            txtPassword.Enabled = false;
+            txtFirstName.Enabled = false;
+            txtLastName.Enabled = false;
+
+            cboUsers.Enabled = true;
+
+            btnCreate.Enabled = true;
+            btnDelete.Enabled = false;
+            btnEdit.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnReturn.Enabled = true;
+
+
+            btnCreate.Text = "Create";
+        }
+
+        private void controlsCreate()
+        {
+            txtUserName.Enabled = true;
+            txtPassword.Enabled = true;
+            txtFirstName.Enabled = true;
+            txtLastName.Enabled = true;
+
+
+            cboUsers.Enabled = false;
+
+            btnCreate.Enabled = true;
+            btnDelete.Enabled = false;
+            btnEdit.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnReturn.Enabled = false;
+            
+
+
+            btnCreate.Text = "Save";
+
+        }
+
+        private void controlsEdit()
+        {
+
+            txtUserName.Enabled = true;
+            txtPassword.Enabled = true;
+            txtFirstName.Enabled = true;
+            txtLastName.Enabled = true;
+
+
+            cboUsers.Enabled = false;
+
+            btnCreate.Enabled = false;
+            btnDelete.Enabled = true;
+            btnEdit.Enabled = false;
+            btnUpdate.Enabled = true;
+            btnReturn.Enabled = false;
+            
+
+        }
+        private void ClearTextBoxes()
+        {
+            txtUserName.Text = "";
+            txtPassword.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+        }
+        private void loadUsers()
+        {
+            cboUsers.DataSource = null;
+            cboUsers.Items.Clear();
+
+            OdbcConnection OdbcConnection = new OdbcConnection();
+            OdbcConnection.ConnectionString = strAccessConnectionString;
+
+            string query = "SELECT username FROM Users";
+            OdbcCommand cmd = new OdbcCommand(query, OdbcConnection);
+
+            OdbcConnection.Open();
+
+            OdbcDataReader dr = cmd.ExecuteReader();
+            AutoCompleteStringCollection UserCollection = new AutoCompleteStringCollection();
+
+            while (dr.Read())
+            {
+                UserCollection.Add(dr.GetString(0));
+
+            }
+
+            OdbcConnection.Close();
+            cboUsers.DataSource = UserCollection;
+           
+        }
+        private void checkIfUsersExists()
+        {
+            string query = "SELECT * FROM Users WHERE UserName='" + txtUserName.Text + "'";
+
+            OdbcConnection OdbcConnection = new OdbcConnection();
+            OdbcCommand cmd;
+            OdbcDataReader dr;
+
+            OdbcConnection.ConnectionString = strAccessConnectionString;
+            OdbcConnection.Open();
+            cmd = new OdbcCommand(query, OdbcConnection);
+            dr = cmd.ExecuteReader();
+            
+
+            if (dr.Read())
+            {
+                boolUserExists = true;
+            }
+            dr.Close();
+            OdbcConnection.Close();
+            dr.Dispose();
+            OdbcConnection.Dispose();
+        }
+        private void editUser()
+        {
+            string query = "SELECT * FROM Users WHERE UserName='" + cboUsers.Text + "'";
+
+            OdbcConnection OdbcConnection = new OdbcConnection();
+            OdbcCommand cmd;
+            OdbcDataReader dr;
+
+            OdbcConnection.ConnectionString = strAccessConnectionString;
+            OdbcConnection.Open();
+            cmd = new OdbcCommand(query, OdbcConnection);
+            dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                intUserID = dr.GetInt32(0);
+                txtUserName.Text = dr.GetString(1);
+                txtPassword.Text = dr.GetString(2);
+               txtFirstName.Text= dr.GetString(3);
+               txtLastName.Text= dr.GetString(4);
+            }
+            
+            dr.Close();
+            OdbcConnection.Close();
+            dr.Dispose();
+            OdbcConnection.Dispose();
+        }
+
+
+        private void createUser()
+        {
+            string query = "SELECT * FROM Users WHERE ID=0";
+            OdbcConnection OdbcConnection = new OdbcConnection();
+            OdbcDataAdapter da = new OdbcDataAdapter(query, OdbcConnection);
+
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            DataRow dr;
+
+            OdbcConnection.ConnectionString = strAccessConnectionString;
+            da.Fill(ds, "Users");
+            dt = ds.Tables["Users"];
+
+            try
+            {
+                dr = dt.NewRow();
+                dr["UserName"] = txtUserName.Text;
+                dr["Password"] = txtPassword.Text;
+                dr["FirstName"] = txtFirstName.Text;
+                dr["LastName"] = txtLastName.Text;
+
+                dt.Rows.Add(dr);
+                OdbcCommandBuilder cmd = new OdbcCommandBuilder(da);
+
+                da.Update(ds, "Users");
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show(EX.Message.ToString());
+            }
+            finally
+            {
+                OdbcConnection.Close();
+                OdbcConnection.Dispose();
+            }
+
+        }
+
+        private void updateUser()
+        {
+            string query = "SELECT * FROM Users WHERE ID=" + intUserID;
+
+            OdbcConnection OdbcConnection = new OdbcConnection();
+            OdbcConnection.ConnectionString = strAccessConnectionString;
+
+            OdbcDataAdapter da = new OdbcDataAdapter(query, OdbcConnection);
+            DataSet ds = new DataSet("Users");
+
+            da.FillSchema(ds, SchemaType.Source, "Users");
+            da.Fill(ds, "Users");
+
+            DataTable dt;
+            dt = ds.Tables["Users"];
+
+            DataRow dr;
+            dr = dt.NewRow();
+
+            try
+            {
+                dr = dt.Rows.Find(intUserID);
+                dr.BeginEdit();
+
+                dr = dt.NewRow();
+                dr["UserName"] = txtUserName.Text;
+                dr["Password"] = txtPassword.Text;
+                dr["FirstName"] = txtFirstName.Text;
+                dr["LastName"] = txtLastName.Text;
+
+                dr.EndEdit();
+
+                OdbcCommandBuilder cmd = new OdbcCommandBuilder(da);
+                da.Update(ds, "Users");
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show(EX.Message.ToString());
+            }
+            finally
+            {
+                OdbcConnection.Close();
+                OdbcConnection.Dispose();
+            }
+
+      
+         
+        }
+        private void deleteUser()
+        {
+            string query = "DELETE FROM Users WHERE ID=" + intUserID;
+
+            OdbcConnection OdbcConnection = new OdbcConnection();
+
+            OdbcCommand cmd;
+            OdbcDataReader dr;
+
+            OdbcConnection.ConnectionString = strAccessConnectionString;
+            OdbcConnection.Open();
+            cmd = new OdbcCommand(query, OdbcConnection);
+            dr = cmd.ExecuteReader();
+           
+            if (dr.Read())
+            {
+
+            }
+            dr.Close();
+            OdbcConnection.Close();
+            dr.Dispose();
+            OdbcConnection.Dispose();
+        }
+           
+
+
+      }
+
+}
